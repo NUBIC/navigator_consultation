7d509a448a05b9c6a857999e48fcdccaa12a5468
randy <randy.janzen@gmail.com>
2014-05-29 09:09:25 -0500

    first commit

---
+++ submit.rb
@@ -0,0 +1,10 @@
+Action:submit
+
+Post form contents to Kayako api
+'Create new ticket' in 'NUCATS Navigator' Queue
+fullname = First name + Last name 
+email = email
+Subject = Consultation request from fullname
+
+Redirect user back to 'Thank you' page on NUCATS site
+
