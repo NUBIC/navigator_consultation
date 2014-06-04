9b33389ac7915ff146ec056281d89e76071fdeee
John Dzak <j-dzak@northwestern.edu>
2014-06-04 14:35:53 -0500

    Hardcoding dev and test kayako configs.

--- config/test.rb
+++ config/test.rb
@@ -3,7 +3,7 @@ require 'bcdatabase'
 b = Bcdatabase.load
 
 KayakoClient::Base.configure do |config|
-  config.api_url    = b['kayako_test', 'api']['url']
-  config.api_key    = b['kayako_test', 'api']['key']
-  config.secret_key = b['kayako_test', 'api']['secret']
+  config.api_url    = 'http://0.0.0.0:9876/api/index.php?'
+  config.api_key    = 'TEST_KEY'
+  config.secret_key = 'TEST_SECRET'
 end
